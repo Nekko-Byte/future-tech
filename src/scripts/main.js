4f9e098b62deb5bc014e26c08d58935e0c318f64
Nikita(Nekko-Byte) <byte.khv.dev@gmail.com>
2025-08-01 22:14:05 +0300

    fix: fix paths and add alias

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 import "../styles/main.scss";
+import Header from "./components/Header.js";
+import TabsCollection from "./components/Tabs.js";
+
+new Header();
+new TabsCollection();
 
 console.log("Hello World");
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite';
+import path from 'path';
 
 export default defineConfig({
-
+  resolve: {
+    alias: {
+      '@helpers': path.resolve(__dirname, './src/styles/helpers'),
+      '@fonts': path.resolve(__dirname, './src/assets/fonts'),
+      '@img': path.resolve(__dirname, './src/assets/images')
+    }
+  }
 })
